bc38b755a3ba0b2148b9d9ef108bb5d903524bc2
Silas Shen <44536021+SighLessShen@users.noreply.github.com>
2019-11-19 14:08:15 -0800

    Client script for scanning and pushing files to FTP server

---
+++ clientScript.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import os
+from ftplib import FTP
+import ftplib
+remoteIP = input("Enter Remote FTP Server IP: ")
+server = remoteIP
+username = 'anonymous'
+ftp_connection = ftplib.FTP(server, username)
+remote_path = "pub"
+nmapdb = input("Enter your name: ")
+subnet =input("Enter IP Address/CIDR for scan: ")
+while True:
+	first = "nmap -n -sn " + subnet + "-oG - | awk '/Up$/{print $2}' > first.txt"
+	second = "nmap -v -T5 " + subnet + " -p21,22,23,25,110,139,443,445,3000,3389,8080 | grep Discovered | awk '{print $6}' > second.txt"
+	os.system(first)
+	os.system(second)
+	os.system("sort first.txt second.txt | uniq > initial.txt")
+	os.system("nmap -sV -T5 -iL initial.txt -oA " + nmapdb)
+	ftp_connection.cwd(remote_path)
+	ftp_connection.set_pasv(False)
+	fh = open(nmapdb +".xml" , 'rb')
+	ftp_connection.storbinary("STOR %s.xml" % nmapdb, fh)
+	fh.close()
+	ftp_connection.quit()
+	
+	
+	
