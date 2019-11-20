a45720d076f67d4feea88290ea12bf4ca0fdcf44
root <root@cpp>
2019-11-20 21:39:25 +0000

    fixed syntax issue

--- clientScript.py
+++ clientScript.py
@@ -2,20 +2,20 @@
 import os
 from ftplib import FTP
 import ftplib
-remoteIP = input("Enter Remote FTP Server IP: ")
 server = remoteIP
 username = 'anonymous'
-ftp_connection = ftplib.FTP(server, username)
 remote_path = "pub"
+remoteIP = input("Enter Remote FTP Server IP: ")
 nmapdb = input("Enter your name: ")
 subnet =input("Enter IP Address/CIDR for scan: ")
 while True:
-	first = "nmap -n -sn " + subnet + "-oG - | awk '/Up$/{print $2}' > first.txt"
+	first = "nmap -n -sn " + subnet + " -oG - | awk '/Up$/{print $2}' > first.txt"
 	second = "nmap -v -T5 " + subnet + " -p21,22,23,25,110,139,443,445,3000,3389,8080 | grep Discovered | awk '{print $6}' > second.txt"
 	os.system(first)
 	os.system(second)
 	os.system("sort first.txt second.txt | uniq > initial.txt")
 	os.system("nmap -sV -T5 -iL initial.txt -oA " + nmapdb)
+        ftp_connection = ftplib.FTP(server, username)
 	ftp_connection.cwd(remote_path)
 	ftp_connection.set_pasv(False)
 	fh = open(nmapdb +".xml" , 'rb')
